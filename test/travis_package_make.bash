712d860d47ab6e92a8719a202666bfdacb513010
Hiro Okamoto <hirowillcom@yahoo.co.jp>
2017-08-14 13:59:24 +0000

    Modyfy .travis.yml

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,10 +1,11 @@
 #!/bin/bash -xve
 
 #sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_ros/
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+#clone pimouse_ros
+cd ~/catkin_ws/src/
+git clone https://github.com/citueda/pimouse_ros.git
+
 cd ~/catkin_ws
 catkin_make
-
-# Copyright 2016 Ryuichi Ueda
-# Released under the BSD License.
-# To make line numbers be identical with the book, this statement is written here. Don't move it to the header.
